487d6b4502e1bbc1f17d5413aeb871c394b256d4
DavidCastro88 <dcastrot@unal.edu.co>
2023-05-15 22:56:04 -0500

    Modificacion Generacion de grafica

--- app/charts.py
+++ app/charts.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 
-def generate_bar_chart(labels,valores):
-    fig,ax=plt.subplots()
-    ax.bar(labels,valores)
+convert_number = lambda number: int(str(number)[0] + ((len(str(number)) - 1) * '0'))
+
+def generate_bar_chart(labels, valores, country=None):
+    fig = plt.figure(figsize=(10, 5))
+    plt.bar(labels, valores, edgecolor='black', linewidth=1.5)
+    plt.title(f'Population in {country} in the last 50 years')
+    plt.xlabel('Year')
+    plt.ylabel('Population(People)')
+    plt.rcParams['font.family'] = 'monospace'
+    plt.legend(['Population'])
+    plt.ticklabel_format(style='plain', axis='y') # Desactiva notación científica en el eje Y
+    plt.ylim(convert_number(list(valores)[0]))
     plt.show()
 
 
--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from read_csv import read_csv
 from charts import generate_bar_chart as chart
 
 
-country=input('Ingrese el pais que desea buscar: ')
+#country=input('Ingrese el pais que desea buscar: ')
+country='Colombia'
 def generate_population_chart_by_country(country):
     #Get data of Population history for countries
     data=read_csv('app/data.csv')
@@ -10,12 +11,12 @@ def generate_population_chart_by_country(country):
         #We filter the country to consult in a list with a single dictionary
         data_country=list(filter(lambda x:x['Country/Territory']==country,data))[0]
         #We get only the keys we need, which have the values of the population in years
-        dic_final={key[0:4:]:data_country[key] for key in data_country if ('Population' in key and 'World' not in key)}
+        dic_final={key[0:4:]:int(data_country[key]) for key in data_country if ('Population' in key and 'World' not in key)}
         #We invert the dictionary so that the population is seen in ascending order
         dic_final = dict(reversed(list(dic_final.items())))
-        chart(dic_final.keys(),dic_final.values())
-    except:
-        print('The country has not been found')
+        chart(list(dic_final.keys()),list(dic_final.values()),country=country)
+    except Exception as e:
+        print(f'Error message: {e}')
 
 generate_population_chart_by_country(country)
 
